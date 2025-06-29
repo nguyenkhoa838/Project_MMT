720fd4525925a9f8d789d55cff7dcdfbfc57aa7a
Võ Nguyên Khoa <vonguyenkhoa838@gmail.com>
2025-06-29 12:46:12 +0700

    update all

--- release/run.bat
+++ release/run.bat
@@ -8,6 +8,7 @@ g++ ../sources/server.cpp ^
     ../sources/shutdown.cpp ^
     ../sources/screen_capture.cpp ^
     ../sources/camera_utils.cpp ^
+    ../sources/screen_record.cpp ^
     -I"C:/msys64/ucrt64/include/opencv4" ^
     -L"C:/msys64/ucrt64/lib" ^
     -lopencv_core -lopencv_imgproc -lopencv_highgui -lopencv_videoio ^
@@ -19,12 +20,13 @@ g++ ../sources/server.cpp ^
 echo [*] Compiling client...
 g++ ../sources/client.cpp ^
     ../sources/camera_utils.cpp ^
+    ../sources/screen_record.cpp ^
     -I"C:/msys64/ucrt64/include/opencv4" ^
     -L"C:/msys64/ucrt64/lib" ^
     -lopencv_core -lopencv_imgproc -lopencv_highgui -lopencv_videoio ^
     -lopencv_imgcodecs -lopencv_video -lopencv_objdetect ^
     -o client.exe ^
-    -lws2_32
+    -lws2_32 -lgdi32 -luser32 -lvfw32
 
 echo [*] Compilation completed.
 pause
