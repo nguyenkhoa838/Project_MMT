2a2129d84737547ed5a2bece3678e7fd1fa6560b
Võ Nguyên Khoa <vonguyenkhoa838@gmail.com>
2025-06-29 12:20:12 +0700

    change webcam

--- release/run.bat
+++ release/run.bat
@@ -1,2 +1,30 @@
-g++ ../sources/server.cpp ../sources/process_utils.cpp ../sources/keylogger.cpp ../sources/restart.cpp ../sources/shutdown.cpp ../sources/screen_capture.cpp ../sources/webcam_capture.cpp -o server.exe -lws2_32 -lgdi32 -luser32 -lvfw32
-g++ ../sources/client.cpp -o client.exe -lws2_32
+@echo off
+echo [*] Compiling server...
+
+g++ ../sources/server.cpp ^
+    ../sources/process_utils.cpp ^
+    ../sources/keylogger.cpp ^
+    ../sources/restart.cpp ^
+    ../sources/shutdown.cpp ^
+    ../sources/screen_capture.cpp ^
+    ../sources/camera_utils.cpp ^
+    -I"C:/msys64/ucrt64/include/opencv4" ^
+    -L"C:/msys64/ucrt64/lib" ^
+    -lopencv_core -lopencv_imgproc -lopencv_highgui -lopencv_videoio ^
+    -lopencv_imgcodecs -lopencv_video -lopencv_objdetect ^
+    -o server.exe ^
+    -lws2_32 -lgdi32 -luser32 -lvfw32
+
+
+echo [*] Compiling client...
+g++ ../sources/client.cpp ^
+    ../sources/camera_utils.cpp ^
+    -I"C:/msys64/ucrt64/include/opencv4" ^
+    -L"C:/msys64/ucrt64/lib" ^
+    -lopencv_core -lopencv_imgproc -lopencv_highgui -lopencv_videoio ^
+    -lopencv_imgcodecs -lopencv_video -lopencv_objdetect ^
+    -o client.exe ^
+    -lws2_32
+
+echo [*] Compilation completed.
+pause
