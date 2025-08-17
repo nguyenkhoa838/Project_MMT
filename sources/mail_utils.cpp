98a807183c52f55e254710c3ee1376bf00ddf4d0
ndeshi <ddthinh2439@clc.fitus.edu.vn>
2025-08-17 19:03:54 +0700

    config

--- sources/mail_utils.cpp
+++ sources/mail_utils.cpp
@@ -331,6 +331,6 @@ void startGmailControlLoop()
             }
         }
 
-        std::this_thread::sleep_for(std::chrono::seconds(1));
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
     }
 }
